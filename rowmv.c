7a8db471c8642eeb858f4157108cf209da7413ef
Your Name <hayri.durmaz@tedu.edu.tr>
2020-12-11 07:49:40 +0300

    int -> double, main changed accordingly, TODO's added

--- rowmv.c
+++ rowmv.c
@@ -2,14 +2,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <omp.h>
 
 #define MATSIZE 2000
 
 static size_t totalMemUsage = 0;
 
-int vectors_dot_prod(int *x, int *y, int n)
+int vectors_dot_prod(double *x, double *y, int n)
 {
-    int res = 0.0;
+    double res = 0.0;
     size_t i;
     for (i = 0; i < n; i++)
     {
@@ -18,7 +19,7 @@ int vectors_dot_prod(int *x, int *y, int n)
     return res;
 }
 
-int vectors_dot_prod2(int *x, int *y, int n)
+int vectors_dot_prod2(double *x, double *y, int n)
 {
     size_t res = 0.0;
     size_t i = 0;
@@ -36,7 +37,7 @@ int vectors_dot_prod2(int *x, int *y, int n)
     return res;
 }
 
-void matrix_vector_mult(int **mat, int *vec, int *result, int rows, int cols)
+void matrix_vector_mult(double **mat, double *vec, double *result, int rows, int cols)
 { // in matrix form: result = mat * vec;
     size_t i;
     for (i = 0; i < rows; i++)
@@ -45,42 +46,42 @@ void matrix_vector_mult(int **mat, int *vec, int *result, int rows, int cols)
     }
 }
 
-int get_random()
+double get_random()
 {
-    int randomNumber = rand() % 100;
+    double randomNumber = rand() / 100;
     // printf("%d\n", randomNumber);
     return randomNumber;
 }
 
-void print_arr(int **arr, int row, int col)
+void print_arr(double **arr, int row, int col)
 {
     size_t i, j;
     for (i = 0; i < row; i++)
     {
         for (j = 0; j < col; j++)
         {
-            printf("%3d ", arr[i][j]);
+            printf("%3f ", arr[i][j]);
         }
         printf("\n");
     }
 }
 
-int **allocarray(int row, int col)
+double **allocarray(int row, int col)
 {
-    int **array = malloc(row * sizeof(int *));
+    double **array = malloc(row * sizeof(double *));
     size_t i, j;
     for (i = 0; i < row; i++)
     {
-        array[i] = malloc(col * sizeof(int));
-        totalMemUsage += col * sizeof(int);
+        array[i] = malloc(col * sizeof(double));
+        totalMemUsage += col * sizeof(double);
         // printf("col=%d - Size of array1[%d]=%d (%d)\n", col, i, sizeof(array1[i]), ((int *)(col * sizeof(int))));
     }
-    totalMemUsage += row * sizeof(int *);
+    totalMemUsage += row * sizeof(double *);
 
     return array;
 }
 
-int **fullfillArrayWithRandomNumbers(int **arr, int row, int col)
+int **fullfillArrayWithRandomNumbers(double **arr, int row, int col)
 {
     /*
     * Fulfilling the array with random numbers 
@@ -93,27 +94,27 @@ int **fullfillArrayWithRandomNumbers(int **arr, int row, int col)
             arr[i][j] = get_random();
         }
     }
-    return arr;
+    return 0;
 }
 
-void print_arr_1d(int *arr, int row)
+void print_arr_1d(double *arr, int row)
 {
     size_t i, j;
     for (i = 0; i < row; i++)
     {
-        printf("%d, ", arr[i]);
+        printf("%f, ", arr[i]);
     }
     printf("\n");
 }
 
-int *allocarray1D(int row)
+double *allocarray1D(int size)
 {
-    int *array = malloc(row * sizeof(int *));
-    totalMemUsage += row * sizeof(int *);
+    double *array = calloc(size, sizeof(double));
+    totalMemUsage += size * sizeof(int);
     return array;
 }
 
-int *fullfillArrayWithRandomNumbers1D(int *arr, int row)
+int *fullfillArrayWithRandomNumbers1D(double *arr, int row)
 {
     /*
     * Fulfilling the array with random numbers 
@@ -123,84 +124,84 @@ int *fullfillArrayWithRandomNumbers1D(int *arr, int row)
     {
         arr[i] = get_random();
     }
-    return arr;
+    return 0;
+}
+
+int ParallelRowMatrixVectorMultiply(int n, double *a, double *b, double *x, MPI_Comm comm)
+{
+    size_t i, j;
+    int nlocal;
+    double *fb;
+    int npes, myrank;
+    MPI_Status status;
+    MPI_Comm_size(comm, &npes);
+    MPI_Comm_rank(MPI_COMM_WORLD, &myrank);
+    fb = (double *)malloc(n * sizeof(double));
+    nlocal = n / npes;
+    MPI_Allgather(b, nlocal, MPI_DOUBLE, fb, nlocal, MPI_DOUBLE, comm);
+    for (i = 0; i < nlocal; i++)
+    {
+        x[i] = 0.0;
+        for (j = 0; j < n; j++)
+        {
+            size_t index = i * n + j;
+            x[i] += a[index] * fb[j];
+        }
+    }
+    free(fb);
+    return 0;
+}
+
+int SequentialMatrixMultiply(int n, double *a, double *b, double *x)
+{
+    size_t i, j;
+    for (i = 0; i < n; i++)
+    {
+        x[i] = 0.0;
+        for (j = 0; j < n; j++)
+        {
+            size_t index = i * n + j;
+            x[i] += a[index] * b[j];
+        }
+    }
+    return 0;
 }
 
 int main(int argc, char *argv[])
 {
     MPI_Init(NULL, NULL);
-    //General declerations
-    int numbers[] = {1000, 2000, 4000, 8000, 16000, 32000};
-    int index = 0;
-    for (index = 0; index < 6; index++)
+    // TODO: matrix allocation
+    int n = 10; //TODO: Take it from arg
+    double *a = allocarray1D(n * n);
+    double *b = allocarray1D(n);
+    double *x = allocarray1D(n);
+    double *xseq = allocarray1D(n);
+
+    if (a == NULL || b == NULL || x == NULL || xseq == NULL)
     {
-        size_t i, j;
-        int N = numbers[index];
-        int K, nOverK;
-        MPI_Status status;
-
-        // Initialize the MPI environment
-
-        // Get the number of processes
-        int world_size;
-        MPI_Comm_size(MPI_COMM_WORLD, &world_size);
-
-        // Get the rank of the process
-        int taskid;
-        MPI_Comm_rank(MPI_COMM_WORLD, &taskid);
-
-        // Get the name of the processor
-        char processor_name[MPI_MAX_PROCESSOR_NAME];
-        int name_len;
-        MPI_Get_processor_name(processor_name, &name_len);
-
-        // Proccess depended inits
-        K = world_size;
-        nOverK = N / K;
-        srand(time(NULL) + taskid);
-
-        // Every proccess creates its part of matrix. (Part 1)
-        int **A_partial = allocarray(nOverK, N);
-
-        A_partial = fullfillArrayWithRandomNumbers(A_partial, nOverK, N);
-        // print_arr(A_partial, nOverK, N);
-
-        // Every proccess creates its part of B and X vectors. (Part 2)
-        int *B_partial = allocarray1D(nOverK);
-        B_partial = fullfillArrayWithRandomNumbers1D(B_partial, nOverK);
-
-        int *X_partial = allocarray1D(nOverK);
-        X_partial = fullfillArrayWithRandomNumbers1D(X_partial, nOverK);
-
-        //ALLGATHER FOR VECTOR
-        int *gatherInput, *B_complete;
-        gatherInput = (int *)malloc(N * sizeof(int));
-        B_complete = (int *)malloc(N * sizeof(int));
-        for (i = 0; i < N; i++)
-            gatherInput[i] = -1;
-        for (i = 0; i < nOverK; i++)
-            gatherInput[taskid * nOverK + i] = B_partial[i];
-
-        MPI_Allgather(&(gatherInput[taskid * nOverK]), nOverK, MPI_INT, B_complete, nOverK, MPI_INT, MPI_COMM_WORLD);
-        // print_arr(A_partial, nOverK, N);
-        // print_arr_1d(B_complete, N);
-
-        MPI_Barrier(MPI_COMM_WORLD);
-        double time_start = MPI_Wtime();
-
-        matrix_vector_mult(A_partial, B_complete, X_partial, nOverK, N);
-        MPI_Barrier(MPI_COMM_WORLD);
-        double time_end = MPI_Wtime();
-        double time = time_end - time_start;
-
-        if (taskid == 0)
-        {
-            totalMemUsage = totalMemUsage / (1024 * 1024);
-            printf("%6d %6d %6f %zu\n", N, world_size, time, totalMemUsage);
-        }
-        // print_arr_1d(X_partial, nOverK);
-        // Finalize the MPI environment.
+        // if (myrank == 0)
+        //     printf("Allocation failed\n");
+        // MPI_Finalize();
+        // return 0;
     }
+
+    // TODO: matrix initialization
+    MPI_Barrier(MPI_COMM_WORLD);
+    double time_start = MPI_Wtime();
+    // TODO: scatter matrix A
+    ParallelRowMatrixVectorMultiply(n, a, b, x, MPI_COMM_WORLD);
+    double time_end = MPI_Wtime();
+    double time = time_end - time_start;
+
+    SequentialMatrixMultiply(n, a, b, xseq);
+    // check difference between x and xseq using OpenMP
+    double time_start_openmp = omp_get_wtime();
+    //TODO: Use, openmp parallel
+    //#pragma omp parallel
+    double time_end_openmp = omp_get_wtime();
+    double time_openmp = time_end_openmp - time_start_openmp;
+    // print matrix size, number of processors, number of threads, time, time_openmp, L2 norm of difference of x and xseq (use %.12e while printing norm)
+
     MPI_Finalize();
     return 0;
 }
